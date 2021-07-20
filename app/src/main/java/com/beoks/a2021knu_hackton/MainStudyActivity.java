af16588c4f3bc43b6ba31907ce392c230ae3a221
BevoLEt <kathor888@gmail.com>
2021-07-20 22:48:47 +0900

    Login, Main, Study 관련 액티비티 레이아웃 및 기본 골격 완성 공모전쪽과 머지 후 디자인 테마 적용 완료

---
+++ app/src/main/java/com/beoks/a2021knu_hackton/MainStudyActivity.java
@@ -0,0 +1,14 @@
+package com.beoks.a2021knu_hackton;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainStudyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_study);
+    }
+}
---
+++ app/src/main/java/com/beoks/a2021knu_hackton/StudyPostActivity.java
@@ -0,0 +1,14 @@
+package com.beoks.a2021knu_hackton;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class StudyPostActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_study_post);
+    }
+}
