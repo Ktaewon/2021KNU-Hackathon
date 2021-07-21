63e381fc81cfd988593b51394079aa93cb53d95b
BevoLEt <kathor888@gmail.com>
2021-07-22 01:05:43 +0900

    제명 이슈 해결

---
+++ app/src/main/java/com/beoks/a2021knu_hackton/StudyPackage/StudyListItem.java
@@ -0,0 +1,22 @@
+package com.beoks.a2021knu_hackton.StudyPackage;
+
+public class StudyListItem {
+    private String date;
+    private String title;
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+}
